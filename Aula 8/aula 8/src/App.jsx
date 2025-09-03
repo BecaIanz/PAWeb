7ca73785f5f5605c805f62442706946af54c5f54
BecaIanz <silveiraianzr@gmail.com>
2025-09-03 11:41:46 -0300

    Segunda parte da aula 8 sobre BrowserRouter

--- Aula 8/aula 8/src/App.jsx
+++ Aula 8/aula 8/src/App.jsx
@@ -1,23 +1,46 @@
 import { useEffect, useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+function Home() {
+  return(
+    <h1>Pagina Inicial</h1>
+  )
+}
+function Sobre() {
+  return(
+    <h1>Sobre a Aplicação</h1>
+  )
+}
 
 function App() {
-  const [num, setNum] = useState(0)
 
-  useEffect(() => {
-    console.log(`mudou para ${num}`)
-  },[num])
   return (
     <>
-    <div className='all'>
-      <button className='button' onClick={() => setNum((num) => num + 1)}>Clique Aqui </button>
-      <h2>{num}</h2>
-    </div>
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/sobre' element={<Sobre></Sobre>}></Route>
+      </Routes>
+      </BrowserRouter>
     </>
   )
+  //Primeira Parte da aula
+  // const [num, setNum] = useState(0)
+
+  // useEffect(() => {
+  //   console.log(`mudou para ${num}`)
+  // },[num])
+  // return (
+  //   <>
+  //   <div className='all'>
+  //     <button className='button' onClick={() => setNum((num) => num + 1)}>Clique Aqui </button>
+  //     <h2>{num}</h2>
+  //   </div>
+      
+  //   </>
+  // )
 }
 
 export default App
